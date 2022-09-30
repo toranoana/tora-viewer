855e37ae6a62177e8271390697e305c958a836c6
Kazuya HATTORI <boushi-bird@users.noreply.github.com>
2022-09-30 17:59:04 +0900

    dist削除

--- dist/types/constants.d.ts
+++
@@ -1,9 +0,0 @@
-export declare const appName = "tora-viewer";
-export declare const pageStyles: readonly ["normal", "spread", "auto"];
-export declare type PageStyle = typeof pageStyles[number];
-export declare const viewerDirections: readonly ["horizontal-ltr", "horizontal-rtl"];
-export declare type ViewerDirection = typeof viewerDirections[number];
-export declare const defaultPageWidth = 840;
-export declare const defaultPageHeight = 1188;
-export declare const defaultLoadImageLimit = 20;
-//# sourceMappingURL=constants.d.ts.map
--- dist/types/interfaces/page-content.d.ts
+++
@@ -1,7 +0,0 @@
-export declare type PageContent = string | {
-    url: string;
-    thumbnailUrl?: string;
-    width?: number;
-    height?: number;
-};
-//# sourceMappingURL=page-content.d.ts.map
